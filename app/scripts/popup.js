3a58a388f6ab6205fcbd65a99eb89918d703a2c2
Kevin Ashworth <kevinashworth@yahoo.com>
2017-02-17 19:49:18 -0800

    popup from showfax-chrome-extention 0.1.2

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,1 +1,33 @@
-console.log('\'Allo \'Allo! Popup');
+document.addEventListener('DOMContentLoaded', function () {
+
+  var input1 = document.getElementById('change_showfax_titles_option');
+
+  // set the initial state of the checkbox
+  chrome.storage.sync.get("change_showfax_titles", function (data) {
+    if (data["change_showfax_titles"]) {
+      input1.checked = true;
+    } else {
+      input1.checked = false;
+    }
+  });
+
+  input1.addEventListener("change", function () {
+    chrome.storage.sync.set({ change_showfax_titles: input1.checked });
+  });
+
+  var input2 = document.getElementById('add_showfax_dropdowns_option');
+
+  // set the initial state of the checkbox
+  chrome.storage.sync.get("add_showfax_dropdowns", function (data) {
+    if (data["add_showfax_dropdowns"]) {
+      input2.checked = true;
+    } else {
+      input2.checked = false;
+    }
+  });
+
+  input2.addEventListener("change", function () {
+    chrome.storage.sync.set({ add_showfax_dropdowns: input2.checked });
+  });
+
+});
