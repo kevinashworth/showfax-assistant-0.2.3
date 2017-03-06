b4eb7e7a4c2e403d9ec8b907ca3ee98bb3e5036f
Kevin Ashworth <kevinashworth@yahoo.com>
2017-03-05 20:33:25 -0800

    Update console text on first load

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,11 +1,17 @@
 // Enable chromereload by uncommenting this line:
-import "chromereload/devonly";
+// import "chromereload/devonly";
 
 // when the extension is first installed
 chrome.runtime.onInstalled.addListener(function (details) { // requires Firefox 52 and up
-  console.log("previousVersion", details.previousVersion);
-  chrome.storage.local.set({ change_showfax_titles: true });
-  chrome.storage.local.set({ add_showfax_dropdowns: true });
+  if (details.reason === "install") {
+    chrome.storage.local.set({ change_showfax_titles: true });
+    chrome.storage.local.set({ add_showfax_dropdowns: true });
+    chrome.storage.local.set({ "showfax_dropdowns['REGIONS']": regionsDropdownDefaultValue });
+    console.log("Thank you for installing" + chrome.i18n.getMessage("appName") + "!");
+  } else if (details.reason === "update") {
+    const thisVersion = chrome.runtime.getManifest().version;
+    console.log("Updating " + chrome.i18n.getMessage("appName") + " from " + details.previousVersion + " to " + thisVersion + "!");
+  }
 });
 
 // listen for any changes to the URL of any tab.
